311dd5e3760886b8bad6ea9bdc10f0aa0928776d
Aleksey090 <yurevi44@gmail.com>
2024-05-05 18:30:44 +0300

    add another refactoring variant

--- app.rb
+++ app.rb
@@ -21,35 +21,45 @@ post '/visit' do
 	@datetime = params[:datetime]
 	@color = params[:color]
 
-		if @username == ''
-			@error = 'Enter name'
-		end
-
-		if @phone == ''
-			@error = 'Enter phone'
-		end
-
-		if @datetime == ''
-			@error = 'Enter time'
-		end
-
-		if @error != ''
-			return erb :visit
-		end
-
-			# -----REFACTORING-------
-		# hh = { :username => 'Enter name',
-				# :phone => 'Enter phone',
-				# :datetime => 'Enter time' }
-		
-		# hh.each do |key, value|
-			# if params[key] == ''
-				# @error = [key]
-				# return erb :visit
-			# end
+				# -----REFACTORING 01-------
+		# if @username == ''
+		# 	@error = 'Enter name'
 		# end
+
+		# if @phone == ''
+		# 	@error = 'Enter phone'
+		# end
+
+		# if @datetime == ''
+		# 	@error = 'Enter time'
+		# end
+
+		# if @error != ''
+		# 	return erb :visit
+		# end
+
+				# -----REFACTORING 02-------
+		hh = { :username => 'Enter name',
+				:phone => 'Enter phone',
+				:datetime => 'Enter time' }
+		
+			@error = hh.select {|key,_| params[key] == ''}.values.join(' & ')
+
+				if @error != ''
+					return erb :visit
+				end
+
+				# -----REFACTORING 03-------
+		# hh.each do |key, value|
+		# 	if params[key] == ''
+		# 		@error = [value]
+		# 		return erb :visit
+		# 	end
+		# end
+
 
 
 	erb "OK, your name: #{@username}, your phone: #{@phone}, date: #{@datetime} and your color: #{@color}"
 
 end
+
